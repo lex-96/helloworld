32381249fffd3209f88b0e6344432dc65b11a34c
lex-96 <lex9612@gmail.com>
2018-02-25 19:26:24 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Version is: " << version() << std::endl;
+	return 0;
+}
---
+++ test_main.cpp
@@ -0,0 +1,12 @@
+#include "lib.h"
+
+#define BOST_TEST_MODULE test_version
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version);
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+	BOOST_CHECK(version() > 0);
+}
