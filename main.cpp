568fbee324e1351e1fe9331edffd1d983e0618ab
lex-96 <lex9612@gmail.com>
2018-02-26 10:08:34 +0300

    main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-int main() {
+int main(int argc, char const *argv[]) {
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "Version is: " << version() << std::endl;
 	return 0;
--- test_main.cpp
+++ test_main.cpp
@@ -1,12 +1,14 @@
 #include "lib.h"
 
-#define BOST_TEST_MODULE test_version
+#define BOST_TEST_MODULE test_main
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version);
+BOOST_AUTO_TEST_SUITE(test_suite_main)
 
-BOOST_AUTO_TEST_CASE(test_valid_version)
+BOOST_AUTO_TEST_CASE(test_version_valid)
 {
-	BOOST_CHECK(version() > 0);
+        BOOST_CHECK( version() > 0 );
 }
+
+BOOST_AUTO_TEST_SUITE_END()
