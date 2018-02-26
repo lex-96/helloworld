f70ef28175574cbef480f3dbf2b29e162e1435f1
lex-96 <lex9612@gmail.com>
2018-02-26 11:51:13 +0300

    test

--- test_main.cpp
+++ test_main.cpp
@@ -4,11 +4,10 @@
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_suite_main);
+BOOST_AUTO_TEST_SUITE(test_version)
 
-BOOST_AUTO_TEST_CASE(test_version_valid)
+BOOST_AUTO_TEST_CASE(test_valid_version)
 {
         BOOST_CHECK( version() > 0 );
 }
 
-BOOST_AUTO_TEST_SUITE_END();
