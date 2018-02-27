872bc95fc52e581d63b1559976971b5da91ecbdc
lex-96 <lex9612@gmail.com>
2018-02-27 20:30:11 +0300

    add version

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, char const *argv[]) {
 	std::cout << "Hello, World!" << std::endl;
-	std::cout << "Version is: " << version() << std::endl;
+        std::cout << "Version is: 0.0." << version() << std::endl;
 	return 0;
 }
